4cd5c98aeb6ff209ef15f598f3accd024742bc8c
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-04-06 09:28:51 -0400

    Commit Modelo MVC creado

---
+++ ExamenPrimerParcial/ExamenPrimerParcial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExamenPrimerParcial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ExamenPrimerParcial/ExamenPrimerParcial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenPrimerParcial.MvcApplication" Language="C#" %>
